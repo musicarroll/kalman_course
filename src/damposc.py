65816e45c47802993b75dc75027ebed344e3ff34
musicarroll <musicarroll@gmail.com>
2023-06-18 18:14:13 -0500

    Made Q dependent on cont. model

--- src/damposc.py
+++ src/damposc.py
@@ -5,7 +5,6 @@ Conversion of damposc.m to python.
 @author: cognotrend
 """
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.linalg import expm
@@ -13,8 +12,8 @@ from collate import collate
 
 np.random.seed(0)  # Set random seed for reproducibility
 
-num_samples = 30
-delta_t = 1.0
+num_samples =200
+delta_t = 0.25
 m = 1
 damp_coeff   = 0.1
 spring_coeff = 0.1
@@ -22,7 +21,8 @@ spring_coeff = 0.1
 Phi = expm(np.array([[0, 1], [-spring_coeff/m, -damp_coeff/m]]) * delta_t)
 
 # Model statistics:
-Q = np.diag([0.1**2, 0.01**2])
+q_cont = 1 # contuous process noise variance
+Q = q_cont*np.array([[delta_t**3/3.0, delta_t**2/2.0],[delta_t**2/2.0,delta_t]])
 P0 = np.diag([0.01**2, 0.1**2])
 R = 0.5**2
 H = np.array([1, 0])
@@ -70,10 +70,10 @@ for k in range(1, num_samples):
 rms_pos = np.sqrt(np.mean((x_true[0, :] - x_hat[0, :])**2))
 rms_vel = np.sqrt(np.mean((x_true[1, :] - x_hat[1, :])**2))
 posQ = f'Pos Q={np.round(Q[0,0],4)}'
-velQ = f'Vel Q={np.round(Q[1,1],4)}'
+velQ = f'Vel Q={np.round(Q[1,1],6)}'
 pos_init_est = f'Pos={np.round(x_hat[0,0],2)}'
 vel_init_est = f'Vel={np.round(x_hat[1,0],2)}'
-param_string = f'Model Init: {pos_init_est}, {vel_init_est}, {posQ}, {velQ},\n Pos P={np.round(P[0,0,0],4)}, Vel P={np.round(P[1,1,0],4)}, R={R}'
+param_string = f'Model Init: {pos_init_est}, {vel_init_est}, {posQ}, {velQ},\n Pos P={np.round(P[0,0,0],4)}, Vel P={np.round(P[1,1,0],4)}, R={np.round(R,6)}'
 fig, axs = plt.subplots(2, 1, figsize=(8, 8))
 times = delta_t*np.arange(1, num_samples+1)
 axs[0].plot(times, x_true[0, :], 'b--*', label='Truth')
