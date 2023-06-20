0ce1c08c992ba61deb0bf4c22e1d7777e408a24c
musicarroll <musicarroll@gmail.com>
2023-06-20 18:05:04 -0500

    Realistic Parameters

--- src/damposc.py
+++ src/damposc.py
@@ -12,26 +12,26 @@ from collate import collate
 
 np.random.seed(0)  # Set random seed for reproducibility
 
-num_samples =200
-delta_t = 0.25
-m = 1
-damp_coeff   = 0.1
-spring_coeff = 0.1
+num_samples =50
+delta_t = 1
+m = 1 # kg
+spring_coeff = 1 # N/m light spring
+damp_coeff   = 5 # Ns/m medium damping
 
 Phi = expm(np.array([[0, 1], [-spring_coeff/m, -damp_coeff/m]]) * delta_t)
 
 # Model statistics:
-q_cont = 1 # contuous process noise variance
+q_cont = 0.01 # contuous process noise variance 
 Q = q_cont*np.array([[delta_t**3/3.0, delta_t**2/2.0],[delta_t**2/2.0,delta_t]])
 P0 = np.diag([0.01**2, 0.1**2])
-R = 0.5**2
+R = 0.10**2
 H = np.array([1, 0])
 x_true = np.zeros((2, num_samples))
 
 # True vs. assumed process noise:
 # Make this parameter=1, if you want the truth and the model to have the same process
 # noise stats.  Otherwise, make it any non-negative value you like for parametric studies.
-process_noise_assumption_factor = 0.0  
+process_noise_assumption_factor = 0.5  
 w1_true = process_noise_assumption_factor * np.sqrt(Q[0, 0]) * np.random.randn(num_samples) 
 w2_true = process_noise_assumption_factor * np.sqrt(Q[1, 1]) * np.random.randn(num_samples)
 w_true = np.vstack((w1_true, w2_true))
@@ -69,11 +69,12 @@ for k in range(1, num_samples):
 
 rms_pos = np.sqrt(np.mean((x_true[0, :] - x_hat[0, :])**2))
 rms_vel = np.sqrt(np.mean((x_true[1, :] - x_hat[1, :])**2))
-posQ = f'Pos Q={np.round(Q[0,0],4)}'
+posQ = f'Pos Q={np.round(Q[0,0],6)}'
 velQ = f'Vel Q={np.round(Q[1,1],6)}'
 pos_init_est = f'Pos={np.round(x_hat[0,0],2)}'
 vel_init_est = f'Vel={np.round(x_hat[1,0],2)}'
 param_string = f'Model Init: {pos_init_est}, {vel_init_est}, {posQ}, {velQ},\n Pos P={np.round(P[0,0,0],4)}, Vel P={np.round(P[1,1,0],4)}, R={np.round(R,6)}'
+
 fig, axs = plt.subplots(2, 1, figsize=(8, 8))
 times = delta_t*np.arange(1, num_samples+1)
 axs[0].plot(times, x_true[0, :], 'b--*', label='Truth')
@@ -88,7 +89,7 @@ axs[0].text(0.5, 0.1, str_text, transform=axs[0].transAxes)
 axs[1].plot(times, x_true[1, :], 'b--*', label='Truth')
 axs[1].plot(times, x_hat[1, :], 'd:k', label='Estimate')
 axs[1].legend(loc='lower left')
-axs[1].set_xlabel(f'Time (s) delta_t ={delta_t}') 
+axs[1].set_xlabel(f'Time (s), $\Delta t$={delta_t}') 
 axs[1].set_ylabel('Velocity (m/sec)')
 str_text = f"RMS Vel Err: {np.round(rms_vel,4)}"
 axs[1].text(0.5, 0.1, str_text, transform=axs[1].transAxes)
@@ -102,7 +103,7 @@ new_time = collate(times, times)
 plt.figure()
 plt.plot(new_time, collated)
 plt.title('Variance in Position Estimate (with Process Noise)\n'+param_string)
-plt.xlabel(f'Time(s) delta_t ={delta_t} ')
+plt.xlabel(f'Time(s), $\Delta t$={delta_t} ')
 plt.ylabel('Variance (m^2)')
 
 plt.show()
