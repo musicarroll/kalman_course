05316ecae0e27431d62ec031d139f55a3164906d
musicarroll <musicarroll@gmail.com>
2023-08-22 12:51:45 -0500

    Modified dampos_musicarroll.py to use tme_subplots
    Also fixed bug in line 50 of movavgs.py

--- damposc_musicarroll.py
+++ damposc_musicarroll.py
@@ -6,6 +6,7 @@ Conversion of damposc.m to python.
 """
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from scipy.linalg import expm
 from collate import collate
@@ -19,14 +20,18 @@ m = 1 # kg
 spring_coeff = 1 # N/m light spring
 damp_coeff   = 5 # Ns/m medium damping
 
-Phi = expm(np.array([[0, 1], [-spring_coeff/m, -damp_coeff/m]]) * delta_t)
+Phi = expm(np.array([[0, 1], 
+        [-spring_coeff/m, -damp_coeff/m]]) * delta_t)
 
 # Model statistics:
 q_cont = 0.01 # contuous process noise variance 
-Q = q_cont*np.array([[delta_t**3/3.0, delta_t**2/2.0],[delta_t**2/2.0,delta_t]])
+Q = q_cont*np.array([[delta_t**3/3.0, delta_t**2/2.0],
+                     [delta_t**2/2.0,delta_t]])
 P0 = np.diag([0.01**2, 0.1**2])
-R = 0.10**2
-H = np.array([1, 0])
+# Measurement noise covariance
+R = np.array([[0.01,0],[0,0.001]])
+# Measurement matrix: Both Position and velocity measurements only
+H = np.eye(2)
 x_true = np.zeros((2, num_samples))
 
 # True vs. assumed process noise:
@@ -49,14 +54,17 @@ P = np.zeros((2, 2, num_samples))
 prior_P = np.zeros((2, 2, num_samples))
 pre = np.zeros((2,num_samples))
 post = np.zeros((2,num_samples))
+z = np.zeros((2,num_samples))
 
-# Pre-compute Measurements:
-v = np.sqrt(R) * np.random.randn(num_samples)
-z = x_true[0,:] + v
+# Initialize measurement sequence (= truth + meas noise):
+z[0,0] = x_true[0, 0] + np.sqrt(R[0,0]) * np.random.randn()
+z[1,0] = x_true[1, 0] + np.sqrt(R[1,1]) * np.random.randn()
 
 P[:, :, 0] = P0
 post[0,0] = P[0, 0, 0]
+post[1,0] = P[1,1,0]
 pre[0,0] = P[0, 0, 0]
+pre[1,0] = P[1, 1, 0]
 
 
 for k in range(1, num_samples):
@@ -64,32 +72,47 @@ for k in range(1, num_samples):
     prior_P[:, :, k] = Phi.dot(P[:, :, k-1]).dot(Phi.T) + Q
     pre[0,k] = prior_P[0, 0, k]
     pre[1,k] = prior_P[1, 1, k]
-    K = prior_P[:, :, k].dot(H.T) / (H.dot(prior_P[:, :, k]).dot(H.T) + R)
-    x_hat[:, k] = prior_est[:, k] + K * (z[k] - H.dot(prior_est[:, k]))
+    K = prior_P[:, :, k].dot(H.T).dot(np.linalg.inv(H.dot(prior_P[:, :, k]).dot(H.T) + R))
+    # K = prior_P[:, :, k].dot(H.T) / (H.dot(prior_P[:, :, k]).dot(H.T) + R)
+    z[0,k] = x_true[0, k] + np.sqrt(R[0,0]) * np.random.randn()
+    z[1,k] = x_true[1, k] + np.sqrt(R[1,1]) * np.random.randn()
+    x_hat[:, k] = prior_est[:, k] + K.dot(z[:,k] - H.dot(prior_est[:, k]))
     P[:, :, k] = (np.eye(2) - K.dot(H)).dot(prior_P[:, :, k]).dot((np.eye(2) - K.dot(H)).T) + K.dot(R).dot(K.T)
+    print(f'************** {k} *****************')
+    print('prior_P:\n',prior_P[:,:,k])
+    print('post_P:\n',P[:,:,k])
     post[0,k] = P[0, 0, k]
     post[1,k] = P[1, 1, k]
 
+
 rms_pos = np.sqrt(np.mean((x_true[0, :] - x_hat[0, :])**2))
 rms_vel = np.sqrt(np.mean((x_true[1, :] - x_hat[1, :])**2))
-posQ = f'Pos Q={np.round(Q[0,0],6)}'
-velQ = f'Vel Q={np.round(Q[1,1],6)}'
+matplotlib.rcParams['axes.titlesize'] = 8
+
+
+posQ = f'Pos Q={np.round(Q[0,0],4)}'
+velQ = f'Vel Q={np.round(Q[1,1],8)}'
 pos_init_est = f'Pos={np.round(x_hat[0,0],2)}'
 vel_init_est = f'Vel={np.round(x_hat[1,0],2)}'
+PosR = np.round(R[0,0],4)
+VelR = np.round(R[1,1],4)
+param_string = f'Model Init: {pos_init_est}, {vel_init_est}, ' \
+                + f'{posQ}, {velQ},\n '\
+                + f'Pos P={np.round(P[0,0,0],4)}, Vel P={np.round(P[1,1,0],4)}, '\
+                + f'Pos R={PosR}, Vel R={VelR}'
 
-param_string = f'Model Init: {pos_init_est}, {vel_init_est}, {posQ}, {velQ},\n Pos P={np.round(P[0,0,0],4)}, Vel P={np.round(P[1,1,0],4)}, R={np.round(R,6)}'
 rms_text = f"RMS Pos Err: {np.round(rms_pos,4)}, RMS Vel Err: {np.round(rms_vel,4)}"
 
-title_string = f'Kalman Estimator: Damped Harmonic Oscillator\n{param_string}\n{rms_text}'
-xlabel_string = f'Time (s), $\Delta t$={delta_t}'
-
+title_string = 'Kalman Estimator: Damped Harmonic Oscillator w/ Pos and Vel Sensors\n'+param_string
+xlabel_string = f'Time (s), $\Delta t$={delta_t}'  
 times = delta_t*np.arange(1, num_samples+1)
 
-meas_array = [z,None]
+meas_array = [z[0,:],z[1,:]]
 
 ylabels =['Position (m)','Velocity (m/sec)']
 tme_subplots(2, times, x_true, meas_array, x_hat, 
                  ylabels, title_string, xlabel_string)
+
 print('Error:')
 print(x_hat[:, -1] - x_true[:, -1])
 
@@ -105,4 +128,4 @@ ylabel_strings = ['Variance (m^2)','Variance ((m/s)^2)']
 
 sawtooth_plots(pre,post,times, title_strings, xlabel_string, ylabel_strings)
 
-
+    
---
+++ movavgs.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from collate import collate
+
+def sma(data,w_size):
+    n=data.size # Assumes data is numpy array
+    smaval = np.zeros(n)
+    gain = 1/w_size
+    for i in range(n):
+        if i<w_size:
+            smaval[i] = data[0:i+1].mean()
+        else:
+            smaval[i] = smaval[i-1] + gain*(data[i] - smaval[i-w_size+1])
+    return smaval
+
+def recursive_moving_avg(meas,gain,prior_est,oldest_meas):
+    avg = prior_est + gain*(meas-oldest_meas)
+    return avg
+
+def tme_plot(plotnum,truth_data, meas_data=None, 
+             est_data=None, ylabel='y', title='', 
+             sigma=1.0,ylim=None):
+    plt.figure(plotnum)
+    plt.plot(truth_data,'.-b')
+    legend = []
+    legend.append('Truth')
+    if type(meas_data)!=type(None):
+        plt.plot(meas_data,'.r')
+        legend.append('Measurements ($\sigma$='+str(sigma)+')')
+    if type(est_data)!=type(None):
+        plt.plot(est_data,'+:k')
+        legend.append('Estimates')
+    plt.xlabel('Time (Epochs)')
+    plt.ylabel(ylabel)
+    plt.legend(legend,loc='lower left')
+    plt.title(title)
+    plt.ylim(ylim)
+    plt.show()
+
+def tme_subplots(numsubplots, times, x_true, meas_array, x_hat, 
+                 ylabels, title_string, xlabel_string):
+    fig, axs = plt.subplots(numsubplots, 1, 
+                figsize=(8, 8),
+                sharex='col', sharey='row'
+                )
+    for i in range(numsubplots):
+        axs[i].plot(times, x_true[i, :], 'b--*', label='Truth')
+        if type(meas_array[i])!=type(None):
+            axs[i].plot(times, meas_array[i], 'r--o', label='Measurements')
+        axs[i].plot(times, x_hat[i, :], 'd:k', label='Estimate')
+        axs[i].legend(loc='lower left')
+        # axs[0].set_xlabel(f'Time (s) $\Delta_t$ ={delta_t}')
+        axs[i].set_ylabel(ylabels[i])
+        if i==0:
+            axs[i].set_title(title_string)
+            
+        axs[i].set_xlabel(xlabel_string) 
+
+def sawtooth_plots(pre,post,
+                   times, title_strings, 
+                   xlabel_string, 
+                   ylabel_string,
+                   plotnumbase=100):        
+    (m,_)=pre.shape
+    
+    new_time = collate(times, times)
+    
+    for i in range(m):
+        collated = collate(pre[0,:], post[0,:])
+        
+        plt.figure(plotnumbase+i)
+        plt.plot(new_time, collated)
+        plt.title(title_strings[i])
+        plt.xlabel(xlabel_string)
+        plt.ylabel(ylabel_string[i])
+        
+        plt.show()
+                    
